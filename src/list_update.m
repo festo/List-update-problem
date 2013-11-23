4f441e19b344929552e5b3192aae6b111a2a7453
festo <gergely.munkacsy@gmail.com>
2013-11-23 14:59:46 +0100

    Add iterations

--- src/list_update.m
+++ src/list_update.m
@@ -1,4 +1,4 @@
-function [ output_args ] = list_update( list_size, request_size )
+function [ output_args ] = list_update( list_size, request_size, iteration )
 
 list = [];
 request = [];
@@ -6,32 +6,25 @@ request = [];
 % random permutation
 list = randperm(list_size);
 
-for i=1:request_size,
-   request(i) = randi(list_size, 1); 
+for it=1:iteration
+
+    for i=1:request_size,
+       request(i) = randi(list_size, 1); 
+    end
+
+    mtf_list = list;
+    mtf_count(it) = MTF(mtf_list, request);
+
+    transpose_list = list;
+    transpose_count(it) = Transpose(transpose_list, request);
+
+    transpose2_list = list;
+    transpose2_count(it) = Transpose2(transpose2_list, request);
+
+    fc_list = list;
+    fc_count(it) = FC(fc_list, request);
+
 end
- 
-mtf_list = list;
-mtf_count = MTF(mtf_list, request);
-mtf_list = [];
 
-mtf_count
-
-transpose_list = list;
-transpose_count = Transpose(transpose_list, request);
-transpose_list = [];
-
-transpose_count
-
-transpose2_list = list;
-transpose2_count = Transpose2(transpose2_list, request);
-transpose2_list = [];
-
-transpose2_count
-
-fc_list = list;
-fc_count = FC(fc_list, request);
-fc_list = [];
-
-fc_count
 end
 
